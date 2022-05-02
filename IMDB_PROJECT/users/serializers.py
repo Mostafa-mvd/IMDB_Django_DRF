8a661fc6e3ebc681c1fd1756e491ec3b78cdc9d3
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2022-05-03 01:34:41 +0430

    added sign up serializer

--- IMDB_PROJECT/users/serializers.py
+++ IMDB_PROJECT/users/serializers.py
@@ -1,3 +1,24 @@
 from rest_framework import serializers
-from . import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+
+class UserSignUpSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super().create(validated_data)
+    
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        del result['password']
+        return result
+        
+    class Meta:
+        model = get_user_model()
+        fields = ('username',
+                  'password', 
+                  'first_name',
+                  'last_name',
+                  'email')
 
