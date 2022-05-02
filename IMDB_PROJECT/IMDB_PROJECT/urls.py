43cfa1c974c91065b909187cda723b7d85b76e7f
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2022-05-02 20:40:37 +0430

    added api apps's urls

--- IMDB_PROJECT/IMDB_PROJECT/urls.py
+++ IMDB_PROJECT/IMDB_PROJECT/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/users/', include('users.urls', namespace='users_ns')),
+    path('api/v1/movies/', include('movies_post.urls', namespace='movies_ns')),
 ]
