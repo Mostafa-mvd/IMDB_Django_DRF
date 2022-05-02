29d52b31459d8b80de0c9e89276a1347490b729e
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2022-05-03 01:40:10 +0430

    added MovieListView for showing all list of movies

--- IMDB_PROJECT/movies_post/views.py
+++ IMDB_PROJECT/movies_post/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from . import models
+from . import serializers
 
-# Create your views here.
+
+class MovieListView(ListAPIView):
+    serializer_class = serializers.MoviesSerializer
+    queryset = models.Movie.objects.all()
+    model = models.Movie
