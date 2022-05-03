aaa263dd029d73733bf3cf38a753fa2f1b09ed10
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2022-05-03 15:51:52 +0430

    Added new views for showing movie detail and sending movie review

--- IMDB_PROJECT/movies_post/views.py
+++ IMDB_PROJECT/movies_post/views.py
@@ -1,9 +1,40 @@
-from rest_framework.generics import ListAPIView
+from django.db import IntegrityError
+from rest_framework.generics import (ListAPIView, 
+                                     CreateAPIView, 
+                                     RetrieveAPIView)
+from rest_framework import permissions
+
+from . import custom_exceptions
 from . import models
 from . import serializers
 
 
-class MovieListView(ListAPIView):
+class MoviesListView(ListAPIView):
     serializer_class = serializers.MoviesListSerializer
     queryset = models.Movie.objects.all()
     model = models.Movie
+
+
+class MovieDetailView(RetrieveAPIView):
+    serializer_class = serializers.MovieDetailSerializer
+    queryset = models.Movie.objects.all()
+    model = models.Movie
+
+
+class MovieReviewView(CreateAPIView):
+    model = models.Review
+    serializer_class = serializers.MovieReviewSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    def create(self, request, *args, **kwargs):
+        # if users want to add another review and rating for movie we have duplicate record so
+        # don't want to add duplicate review for one movie by users
+        try:
+            return super().create(request, *args, **kwargs)
+        except IntegrityError:
+            raise custom_exceptions.DuplicateReviewException
+
+    def get_serializer_context(self):
+        serializer_context = super().get_serializer_context()
+        serializer_context['pk'] = self.kwargs.get("pk", None)
+        return serializer_context
