556f124aa9f915aaa0550338f2492bb50dffcca8
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2022-05-03 15:51:00 +0430

    Added new urls for movie detail and sending review

--- IMDB_PROJECT/movies_post/urls.py
+++ IMDB_PROJECT/movies_post/urls.py
@@ -6,9 +6,10 @@ from . import views
 
 app_name = "movies"
 
-
 urlpatterns = [
-    path('list/', views.MovieListView.as_view(), name='movies_list'),
+    path('list/', views.MoviesListView.as_view(), name='movies_list'),
+    path('<int:pk>/', views.MovieDetailView.as_view(), name='movie_detail'),
+    path('<int:pk>/review/', views.MovieReviewView.as_view(), name='movie_review'),
 ]
 
 if settings.DEBUG:
